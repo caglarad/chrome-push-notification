3bb4cedbbe97bceacb2fb52ca1c0701ba57ab47b
ykptke <ykpteke@gmail.com>
2016-05-13 14:37:48 +0300

    version update

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'proyk:chrome-push-notification',
-  version: '0.0.1',
+  version: '0.0.2',
   summary: 'Chrome push notifications',
   git: '',
   documentation: 'README.md'
